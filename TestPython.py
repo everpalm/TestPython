609401034340709422736a6233081268ccf54cf1
Ting Jung Cheng <jaron@alumni.ncu.edu.tw>
2017-07-16 10:18:00 +0800

    CTJ170716 Trial of basic IO

--- TestPython.py
+++ TestPython.py
@@ -1,1 +1,16 @@
 print "Hello, world!"
+import sys
+
+#MyWrite = open('test.txt','w')
+#MyWrite.write('No more check')
+#MyWrite.write('Replicate your soul')
+#MyWrite.close()
+
+#MyFile = open('test.txt','r')
+#MyContent = MyFile.read()
+#print MyContent
+#MyFile.close()
+#def readfile(test)
+
+#for line in open('test.txt','r'):
+#    print line
