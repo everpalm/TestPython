afe8b1f0541fdcf25876e0bb9f7fa1997a8d1899
Ting Jung Cheng <jaron@alumni.ncu.edu.tw>
2017-09-02 14:52:37 +0800

    CTJ170902 MyRead.py	Practical operation on input a file

--- MyRead.py
+++ MyRead.py
@@ -1,2 +1,4 @@
+
+''' This is example of input a file '''
 for line in open('test.txt','r'):
     print line
--- TestPython.py
+++ TestPython.py
@@ -1,5 +1,13 @@
-print "Hello, world!"
 import sys
+import logging
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print "Hello, world!"
+
+line = input("integer: ")
+print("line = ", line)
 
 #MyWrite = open('test.txt','w')
 #MyWrite.write('No more check')
@@ -14,3 +22,60 @@ import sys
 
 #for line in open('test.txt','r'):
 #    print line
+
+#MyFile = open(sys.argv[1],'r')
+#MyContent = MyFile.read()
+#print MyContent
+
+#import os
+#cmd = 'python MyRead.py'
+#os.system(cmd)
+#print os.getcwd()
+
+
+# Plot excercise
+'''
+
+
+
+normal_samples = np.random.normal(size = 10000)
+uniform_samples = np.random.uniform(size = 1000000)
+plt.hist(normal_samples)
+plt.show()
+'''
+
+'''
+
+logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.WARNING)
+logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger(__name__)
+
+text_a = 10
+logger.debug('This is a test %d' % (text_a))
+
+logger.warning('What the hell')
+
+logger.info('Check this out')
+
+
+def my_test(x):
+    if x == 0:
+        def test_define(y):
+            logger.info('Find my_test()')
+        print(test_define(1))
+        
+        return 'My godness'
+    else:
+        return 'God'
+
+print my_test(0)
+print my_test(1)
+'''
+
+'''
+afa = input('afa =')
+beta = input('beta =')
+print('afa = %d, beta = %d' % (afa, beta))
+'''
